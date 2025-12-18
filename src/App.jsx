889dae4d9346c2aba76d563787ebabf3f22ae67e
Swastik Shetty <127413152+SwastikShetty06@users.noreply.github.com>
2025-12-18 12:32:04 +0530

    Update in Routhing

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const Placeholder = ({ title }) => (
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
